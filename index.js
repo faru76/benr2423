05fe9b8b2f5d399ea0b80cfde3f8ded484f74c31
faru76 <khanfairuz764@gmail.com>
2023-03-27 15:19:42 +0800

    array and object exercise

---
+++ index.js
@@ -0,0 +1,24 @@
+// Exercise of numbers and arrays 
+// let data1 = true
+// let data2 ="1.25"
+// console.log(data1,typeof data1)
+// console.log(data2,typeof data2)
+
+// let data3 = data1.toString() + data2  //parseInt or ParseFloat or .toString() to change data type
+// console.log(data3, typeof data3)
+
+//Array
+let data = ["one",2,true]
+console.log(data[2], typeof data[2])
+
+// Object
+let user = {
+    name:"faru",
+    faculty:"fkekk",
+    phone:{
+        office:"0108849764",
+        mobile:"0108849765"
+    },
+    email:"faru@mail.com"
+}
+console.log(user.phone.mobile)
